5ff15e19e8b785e7f427d67bca83a69cbe7e8bee
Victor Arsjad <victor.arsjad@go-jek.com>
2021-10-12 13:12:18 +0700

    revamp navigate function, refactor code

---
+++ src/code.ts
@@ -0,0 +1,32 @@
+import { GlobPattern, TextEditor, Uri, window, workspace } from "vscode";
+import { getFileDetail } from "./utils";
+
+export class Vscode {
+	public getFocusedEditor = () => {
+		return window.activeTextEditor;
+	};
+
+	public getFilePath = (editor: TextEditor) => {
+		return editor.document.fileName;
+	};
+
+	public getFocusedFilePath = () => {
+		const editor = this.getFocusedEditor();
+		if (!editor) {
+			return;
+		}
+		return this.getFilePath(editor!!);
+	};
+
+	public getTerminal() {
+		return window.activeTerminal || window.createTerminal();
+	}
+
+	public showErrorMessage(message: string) {
+		window.showErrorMessage(`Ouranos: ${message}`);
+	}
+
+	public findFiles(include: GlobPattern): Thenable<Uri[]> {
+		return workspace.findFiles(include, "**/.elixir_ls/**");
+	}
+}
